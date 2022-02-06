dad9e4a670c8f0a48d926621f44d4133af928cbb
Addison MacPhee <amacphee04@gmail.com>
2022-02-06 15:08:06 -0500

    Added comments.

--- tree.c
+++ tree.c
@@ -4,13 +4,12 @@
 #include <string.h>
 
 void print_line(char *pid, int depth) {
-    // open stat file for pid
+    // open stat file for current process
     FILE *fp;
     char *filename;
     asprintf(&filename, "/proc/%s/stat", pid);
     fp = fopen(filename, "r");
     free(filename);
-
     char file[200000];
     if (fp == NULL) {
     	printf("stat: No such file.\n");
@@ -25,7 +24,7 @@ void print_line(char *pid, int depth) {
     vsize = (char *) malloc(100);
     comm = (char *) malloc(1000);
 
-    // scan & gather info
+    // scan & gather info from stat file
     char *save;
     char* token = strtok_r(file, " ", &save);
     int i = 0;
@@ -41,19 +40,17 @@ void print_line(char *pid, int depth) {
 	for (int j = 0; j < depth; j++) {printf("  ");}
     printf("%s %s, %s kb\n", pid, comm, vsize);
 
-    // open child file
+    // open children file
     FILE *cf;
     char *chi_filename;
     asprintf(&chi_filename, "/proc/%s/task/%s/children", pid, pid);
     cf = fopen(chi_filename, "r");
     free(chi_filename);
-
-    // loop through and call print_line on all children
     char chi_file[200000];
     fgets(chi_file, sizeof(chi_file), cf);
     fclose(cf);
 
-    // base case: no kids
+    // loop through and call print_line on all children via recursion
     if (chi_file == NULL) {
     	printf("chi_file: No such file. \n");
     } else {
@@ -65,12 +62,14 @@ void print_line(char *pid, int depth) {
     	}
     }
 
+    // free malloc'd variables
     free(ppid);
     free(vsize);
     free(comm);
 }
 
 int main(int argc, char **argv) {
+    // start tree with process 1
     print_line("1", 0);
     return 0;
 }
